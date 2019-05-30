d77c21d2e6931b8063f3a650c05ccfc487d5b396
Faisal <faisal@rotatestudios.com>
2019-05-30 00:48:13 -0500

    Updates

---
+++ precache-manifest.f3a7f6f6490be694ec12127753dcd269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce85a90a99071b5ee179cecc1b020ccb",
+    "url": "/rotatestudios.github.io/meme-generator/index.html"
+  },
+  {
+    "revision": "449abf4fa64921b8b976",
+    "url": "/rotatestudios.github.io/meme-generator/static/css/main.a2030869.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/rotatestudios.github.io/meme-generator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "449abf4fa64921b8b976",
+    "url": "/rotatestudios.github.io/meme-generator/static/js/main.3310fa0e.chunk.js"
+  },
+  {
+    "revision": "ecd649e8c5a0df28f3e1",
+    "url": "/rotatestudios.github.io/meme-generator/static/js/runtime~main.136f078d.js"
+  }
+]);
